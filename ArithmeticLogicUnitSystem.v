99e81b1dc5e0f2a806f60540cbf1e6e8adfe52e6
Arif Eren Yoldaş <yoldas21@itu.edu.tr>
2025-07-17 23:01:48 +0300

    /

--- ArithmeticLogicUnitSystem.v
+++ ArithmeticLogicUnitSystem.v
@@ -22,8 +22,15 @@ module Mux4to1_32bit (
     input wire [31:0] in0, in1, in2, in3,
     input wire [1:0] sel,
     output wire [31:0] out
-);
+    );
     assign out = (sel == 2'b00) ? in0 :
                  (sel == 2'b01) ? in1 :
                  (sel == 2'b10) ? in2 : in3;
+endmodule
+
+
+module moduleName (
+    ports
+);
+    
 endmodule
