488d74544e20f4c592b0c223f3ab7280f822c829
Arif Eren Yoldaş <yoldas21@itu.edu.tr>
2025-07-17 07:08:13 +0300

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -1,75 +1,79 @@
-call C:\Xilinx\Vivado\2017.4\settings64.bat
+#!/bin/bash
 
-set folder=Simulation_Files
-mkdir %folder%
-copy "RAM.mem" "%folder%/RAM.mem"
-cd "%folder%
+# Source Vivado environment (Linux path!)
+source /opt/Xilinx/Vivado/2017.4/settings64.sh
 
-::Register 16 Simulation
-call xvlog ../Register16bit.v  
-call xvlog ../Register16bitSimulation.v
-call xvlog ../Helper.v
-call xelab -top Register16bitSimulation -snapshot reg16sim -debug typical
-call xsim reg16sim -R
+# Create simulation folder
+folder="Simulation_Files"
+mkdir -p "$folder"
+cp RAM.mem "$folder/RAM.mem"
+cd "$folder" || exit
 
-::Register 32 Simulation
-call xvlog ../Register32bit.v  
-call xvlog ../Register32bitSimulation.v
-call xvlog ../Helper.v
-call xelab -top Register32bitSimulation -snapshot reg32sim -debug typical
-call xsim reg32sim -R
+# --- Register 16 Simulation ---
+xvlog ../Register16bit.v  
+xvlog ../Register16bitSimulation.v
+xvlog ../Helper.v
+xelab -top Register16bitSimulation -snapshot reg16sim -debug typical
+xsim reg16sim -R
 
-::Register File Simulation
-call xvlog ../Register32bit.v  
-call xvlog ../RegisterFile.v  
-call xvlog ../RegisterFileSimulation.v
-call xvlog ../Helper.v
-call xelab -top RegisterFileSimulation -snapshot regfilesim -debug typical
-call xsim regfilesim -R
+# --- Register 32 Simulation ---
+xvlog ../Register32bit.v  
+xvlog ../Register32bitSimulation.v
+xvlog ../Helper.v
+xelab -top Register32bitSimulation -snapshot reg32sim -debug typical
+xsim reg32sim -R
 
-::Address Register File Simulation
-call xvlog ../Register16bit.v  
-call xvlog ../AddressRegisterFile.v  
-call xvlog ../AddressRegisterFileSimulation.v
-call xvlog ../Helper.v
-call xelab -top AddressRegisterFileSimulation -snapshot addregfilesim -debug typical
-call xsim addregfilesim -R
+# --- Register File Simulation ---
+xvlog ../Register32bit.v  
+xvlog ../RegisterFile.v  
+xvlog ../RegisterFileSimulation.v
+xvlog ../Helper.v
+xelab -top RegisterFileSimulation -snapshot regfilesim -debug typical
+xsim regfilesim -R
 
-::Instruction Register Simulation
-call xvlog ../InstructionRegister.v  
-call xvlog ../InstructionRegisterSimulation.v
-call xvlog ../Helper.v
-call xelab -top InstructionRegisterSimulation -snapshot insregsim -debug typical
-call xsim insregsim -R
+# --- Address Register File Simulation ---
+xvlog ../Register16bit.v  
+xvlog ../AddressRegisterFile.v  
+xvlog ../AddressRegisterFileSimulation.v
+xvlog ../Helper.v
+xelab -top AddressRegisterFileSimulation -snapshot addregfilesim -debug typical
+xsim addregfilesim -R
 
-::Data Register Simulation
-call xvlog ../DataRegister.v  
-call xvlog ../DataRegisterSimulation.v
-call xvlog ../Helper.v
-call xelab -top DataRegisterSimulation -snapshot dataregsim -debug typical
-call xsim dataregsim -R
+# --- Instruction Register Simulation ---
+xvlog ../InstructionRegister.v  
+xvlog ../InstructionRegisterSimulation.v
+xvlog ../Helper.v
+xelab -top InstructionRegisterSimulation -snapshot insregsim -debug typical
+xsim insregsim -R
 
-::Arithmetic Logic Unit Simulation
-call xvlog ../ArithmeticLogicUnit.v  
-call xvlog ../ArithmeticLogicUnitSimulation.v
-call xvlog ../Helper.v
-call xelab -top ArithmeticLogicUnitSimulation -snapshot alusim -debug typical
-call xsim alusim -R
+# --- Data Register Simulation ---
+xvlog ../DataRegister.v  
+xvlog ../DataRegisterSimulation.v
+xvlog ../Helper.v
+xelab -top DataRegisterSimulation -snapshot dataregsim -debug typical
+xsim dataregsim -R
 
-::Arithmetic Logic Unit System Simulation
-call xvlog ../Register16bit.v  
-call xvlog ../Register32bit.v  
-call xvlog ../RegisterFile.v
-call xvlog ../AddressRegisterFile.v  
-call xvlog ../InstructionRegister.v
-call xvlog ../DataRegister.v  
-call xvlog ../ArithmeticLogicUnit.v
-call xvlog ../Memory.v  
-call xvlog ../ArithmeticLogicUnitSystem.v  
-call xvlog ../ArithmeticLogicUnitSystemSimulation.v
-call xvlog ../Helper.v
+# --- Arithmetic Logic Unit Simulation ---
+xvlog ../ArithmeticLogicUnit.v  
+xvlog ../ArithmeticLogicUnitSimulation.v
+xvlog ../Helper.v
+xelab -top ArithmeticLogicUnitSimulation -snapshot alusim -debug typical
+xsim alusim -R
 
-call xelab -top ArithmeticLogicUnitSystemSimulation -snapshot alusyssim -debug typical
-call xsim alusyssim -R
+# --- Arithmetic Logic Unit System Simulation ---
+xvlog ../Register16bit.v  
+xvlog ../Register32bit.v  
+xvlog ../RegisterFile.v
+xvlog ../AddressRegisterFile.v  
+xvlog ../InstructionRegister.v
+xvlog ../DataRegister.v  
+xvlog ../ArithmeticLogicUnit.v
+xvlog ../Memory.v  
+xvlog ../ArithmeticLogicUnitSystem.v  
+xvlog ../ArithmeticLogicUnitSystemSimulation.v
+xvlog ../Helper.v
+
+xelab -top ArithmeticLogicUnitSystemSimulation -snapshot alusyssim -debug typical
+xsim alusyssim -R
 
 cd ..
