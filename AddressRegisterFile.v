a5c67e90c54c6abe2cd865f12aa4d2f4fe06d9ec
Arif Eren Yoldaş <yoldas21@itu.edu.tr>
2025-07-23 14:20:20 +0300

    A few wires are added for a better visualization

--- AddressRegisterFile.v
+++ AddressRegisterFile.v
@@ -21,11 +21,11 @@ module AddressRegisterFile(
     Register16bit AR(.I(I[15:0]), .E(AR_E), .FunSel(FunSel), .Clock(Clock), .Q(Q_AR));
     Register16bit SP(.I(I[15:0]), .E(SP_E), .FunSel(FunSel), .Clock(Clock), .Q(Q_SP));
 
-    assign OutC = (OutCSel == 2'b00) ? PC.Q :
-                  (OutCSel == 2'b01) ? SP.Q : AR.Q;
+    assign OutC = (OutCSel == 2'b00) ? Q_PC :
+                  (OutCSel == 2'b01) ? Q_SP : Q_AR;
                   
-    assign OutD = (OutDSel == 2'b00) ? PC.Q :
-                  (OutDSel == 2'b01) ? SP.Q : AR.Q;
+    assign OutD = (OutDSel == 2'b00) ? Q_PC :
+                  (OutDSel == 2'b01) ? Q_SP : Q_AR;
 
     // always @(posedge Clock) begin
     //     if (we) begin
